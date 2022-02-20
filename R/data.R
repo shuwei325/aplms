fe3744c3a47ea6a656367820730c748994029dc6
shuwei325 <shuwei.chou@ucr.ac.cr>
2022-02-20 14:56:22 -0600

    update some metadata

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Global Annual Mean Surface Air Temperature Change
+#'
+#' Land-ocean temperature index from 1880--2021 (with base period 1951-1980).
+#'
+#'
+#' @format Time series data
+#' @source NASA/GISS/GISTEMP
+#' @keywords datasets
+#' @examples
+#' plot(temperature)
+#'
+"temperature"
