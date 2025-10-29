da287d5626874d119e76479a006c28cdb93755f3
shuwei325 <shuwei.chou@ucr.ac.cr>
2025-10-29 10:28:48 -0600

    Update influence.aplms and corrections

--- R/influence.aplms.R
+++ R/influence.aplms.R
@@ -28,32 +28,29 @@ influence.aplms <- function(model,
 
 
   list_output <- list()
-
   if (any(perturbation == "case-weight")){
-    list_output <- perturbation_case1(model, list_output, "case-weight")
+    list_output[[which(perturbation=="case-weight")]] <- perturbation_case1(model, "case-weight", part)
   }
 
   if (any(perturbation == "dispersion")){
-    list_output <- perturbation_case1(model, list_output, "dispersion")
+    list_output[[which(perturbation=="dispersion")]] <- perturbation_case1(model, "dispersion", part)
   }
 
   if (any(perturbation == "response")){
-    list_output <- perturbation_case1(model, list_output, "response")
+    list_output[[which(perturbation=="response")]] <- perturbation_case1(model, "response", part)
   }
 
   if (any(perturbation == "explanatory")){
     N_i <- model$N_i
     exp_index <- 1:(sapply(N_i,ncol)[1]-1)
-    list_output[[which(perturbation=="explanatory")]] <- list()
-    list_output <- perturbation_case2(model, list_output, "explanatory", exp_index)
-    names(list_output[[which(perturbation=="explanatory")]]) <- row.names(model$f[[1]])[-1]
+    list_output[[which(perturbation=="explanatory")]] <- perturbation_case2(model, "explanatory", exp_index, part)
+    names(list_output[[which(perturbation=="explanatory")]]) <- attr(stats::terms(model$formula), "term.labels")
   }
 
   if (any(perturbation == "corAR")){
     p <- nrow(model$summary_table_phirho)-1
     k_index <- 1:p
-    list_output[[which(perturbation=="corAR")]] <- list()
-    list_output <- perturbation_case2(model, list_output, "corAR", k_index)
+    list_output[[which(perturbation=="corAR")]] <- perturbation_case2(model, "corAR", k_index, part)
     names(list_output[[which(perturbation=="corAR")]]) <- paste0("rho",1:p)
   }
 
@@ -64,10 +61,10 @@ influence.aplms <- function(model,
 # Returns the conformal normal curvature of the fitted `aplms` model object. The `case-weight`, `dispersion`, `response` perturbations are available.
 #
 # @param model an object with the result of fitting additive partial linear models with symmetric errors.
-# @param list_output an empty list object that will contain the conformal normal curvature of the specified perturbations.
 # @param perturbation A string vector specifying a perturbation scheme: `case-weight`, `dispersion`, `response`.
+# @param part A logical value to indicate whether the influential analysis is performed for \eqn{\gamma}, \eqn{\phi} and \eqn{\rho}.
 # @return A list object containing the conformal normal curvature of the specified perturbations.
-perturbation_case1 <- function(model, list_output, perturbation) {
+perturbation_case1 <- function(model, perturbation, part) {
   p <- nrow(model$summary_table_phirho)-1
   N_i <- model$N_i
   Lobs <- model$LL_obs
@@ -78,8 +75,8 @@ perturbation_case1 <- function(model, list_output, perturbation) {
 
   DELTA <- influence_DELTA(model = model, perturb_scheme = perturbation)
   C_i <- conf_normal_curvature(DELTA, Lobs, Lobs.aux = 0)
-  list_output[[which(perturbation==perturbation)]] <- list()
-  list_output[[which(perturbation==perturbation)]]$C_i <- C_i
+  list_output <- list()
+  list_output$C_i <- C_i
   if(part){
     # gamma
     Lobs.aux_gamma <- Gp(Lobs, index = gamma_index)
@@ -90,9 +87,9 @@ perturbation_case1 <- function(model, list_output, perturbation) {
     # rhos
     Lobs.aux_rhos <- Gp(Lobs, index = rho_index)
     C_i_rhos <- conf_normal_curvature(DELTA,Lobs, Lobs.aux = Lobs.aux_rhos)
-    list_output[[which(perturbation==perturbation)]]$C_i_gamma <- C_i_gamma
-    list_output[[which(perturbation==perturbation)]]$C_i_phi <- C_i_phi
-    list_output[[which(perturbation==perturbation)]]$C_i_rhos <- C_i_rhos
+    list_output$C_i_gamma <- C_i_gamma
+    list_output$C_i_phi <- C_i_phi
+    list_output$C_i_rhos <- C_i_rhos
   }
   return(list_output)
 }
@@ -100,10 +97,10 @@ perturbation_case1 <- function(model, list_output, perturbation) {
 # Returns the conformal normal curvature of the fitted `aplms` model object. The  `explanatory` and `corAR`. perturbations are available.
 #
 # @param model an object with the result of fitting additive partial linear models with symmetric errors.
-# @param list_output an empty list object that will contain the conformal normal curvature of the specified perturbations.
-# @param perturbation A string vector specifying a perturbation scheme:  `explanatory` and `corAR`..
+# @param perturbation A string vector specifying a perturbation scheme:  `explanatory` and `corAR`.
+# @param part A logical value to indicate whether the influential analysis is performed for \eqn{\gamma}, \eqn{\phi} and \eqn{\rho}.
 # @return A list object containing the conformal normal curvature of the specified perturbations.
-perturbation_case2 <- function(model, list_output, perturbation, iteration) {
+perturbation_case2 <- function(model, perturbation, iteration, part) {
   p <- nrow(model$summary_table_phirho)-1
   N_i <- model$N_i
   Lobs <- model$LL_obs
@@ -112,12 +109,13 @@ perturbation_case2 <- function(model, list_output, perturbation, iteration) {
   phi_index <- sum(coef_index)-p
   rho_index <- ((sum(coef_index)-p+1):sum(coef_index))
 
+  list_output <- list()
   for (i in iteration){
     if (perturbation == "explanatory") DELTA <- influence_DELTA(model = model, perturb_scheme = "explanatory", r = i)
     if (perturbation == "corAR") DELTA <- influence_DELTA(model = model, perturb_scheme = "corAR", k = i)
     C_i <- conf_normal_curvature(DELTA, Lobs, Lobs.aux = 0)
-    list_output[[which(perturbation==perturbation)]][[i]] <- list()
-    list_output[[which(perturbation==perturbation)]][[i]]$C_i <- C_i
+    list_output[[i]] <- list()
+    list_output[[i]]$C_i <- C_i
     if(part){
       # gamma
       Lobs.aux_gamma <- Gp(Lobs, index = gamma_index)
@@ -128,9 +126,9 @@ perturbation_case2 <- function(model, list_output, perturbation, iteration) {
       # rhos
       Lobs.aux_rhos <- Gp(Lobs, index = rho_index)
       C_i_rhos <- conf_normal_curvature(DELTA,Lobs, Lobs.aux = Lobs.aux_rhos)
-      list_output[[which(perturbation==perturbation)]][[i]]$C_i_gamma <- C_i_gamma
-      list_output[[which(perturbation==perturbation)]][[i]]$C_i_phi <- C_i_phi
-      list_output[[which(perturbation==perturbation)]][[i]]$C_i_rhos <- C_i_rhos
+      list_output[[i]]$C_i_gamma <- C_i_gamma
+      list_output[[i]]$C_i_phi <- C_i_phi
+      list_output[[i]]$C_i_rhos <- C_i_rhos
     }
   }
   return(list_output)
